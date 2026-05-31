4e40c286bad126b33563cc0ee13f642ec77f7e93
Vishal Jha <vjha93011@gmail.com>
2026-05-31 23:22:06 +0530

    use gemini api

--- agentic_1/index.js
+++ agentic_1/index.js
@@ -1,14 +1,31 @@
-import { OpenAI } from "openai/client.js";
+import { GoogleGenAI } from "@google/genai";
+import { configDotenv } from "dotenv";
 
-const client = OpenAI({
-    apikey:process.env.OPEN_API
-})
+configDotenv();
 
-function getWeatherDetail(city = ''){
-    if(city.toLowerCase() === 'patiala') return '10°C';
-    if(city.toLowerCase() === 'chandigarh') return '30°C';
-    if(city.toLowerCase() === 'delhi') return '45°C';
-    if(city.toLowerCase() === 'banglore') return '34°C';
-    if(city.toLowerCase() === 'pune') return '15°C';
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
+function getWeatherDetail(city = "") {
+  if (city.toLowerCase() === "patiala") return "10°C";
+  if (city.toLowerCase() === "chandigarh") return "30°C";
+  if (city.toLowerCase() === "delhi") return "45°C";
+  if (city.toLowerCase() === "bangalore") return "34°C";
+  if (city.toLowerCase() === "pune") return "15°C";
+
+  return "Weather data not found";
 }
+
+const user = "Hey, What is the weather of Patiala";
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: user,
+  });
+
+  console.log(response.text);
+}
+
+main();
